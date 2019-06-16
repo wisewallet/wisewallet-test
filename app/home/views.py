0d84254766226fbeea2b2fba7599487bc8e44f3f
Vrajesh Mehta <vmehta342@gmail.com>
2019-06-16 19:53:06 -0400

    Adding functionality to search by company name , category and causes

--- app/home/views.py
+++ app/home/views.py
@@ -1,10 +1,11 @@
 from flask import render_template
 from flask_login import current_user,login_required
 from forms import SearchForm
-from ..models import Property,Company,CompanyHasProperty,Usersdatas
+from ..models import Property,Company,CompanyHasProperty
 from flask import flash, render_template, request, redirect
 from . import home
 from flask import jsonify
+from ..utils.search_functions import *
 
 
 @home.route('/')
@@ -60,12 +61,14 @@ def dashboard():
     #                         com_dict=com_dict,
     #                         title="Dashboard")
 
+
 @home.route('/admin/dashboard')
 @login_required
 def admin_dashboard():
     if not current_user.is_admin:
         abor(403)
-    return render_template('home/admin_dashboard.html',title="Dashboard")
+    return render_template('home/admin_dashboard.html', title="Dashboard")
+
 
 @home.route('/company/<company_name>',methods=['GET','POST'])
 @login_required
@@ -101,6 +104,7 @@ def get_companies_by_name(company_name):
     #                         company_pname_list=company_pname_list,
     #                         property_list=property_list,title=company.name)
 
+
 @home.route('/property',methods=['GET'])
 def get_property_data():
     final_Data = []
@@ -118,43 +122,82 @@ def get_property_data():
     })
     return response
 
-@home.route('/search',methods=['GET', 'POST'])
+# @home.route('/search',methods=['GET', 'POST'])
+# @login_required
+# def search():
+#     # form = SearchForm()
+#     data = request.get_json()
+#     search = data['search']
+#     company = Company.query.filter(Company.name.like('%' + search + '%'))
+#     property =  Property.query.with_entities(Property.name).all()
+#     property_list = [value for value, in property]
+#     com_dict = {}
+#     data = {}
+#     final_Data = []
+#     for com in company:
+#         companyHasproperty = CompanyHasProperty.query.with_entities(
+#             CompanyHasProperty.p_id).filter(
+#                 CompanyHasProperty.c_id == com.id).all()
+#         companyHasproperty_pid = [value for value, in companyHasproperty]
+#         company_pid_list = companyHasproperty_pid
+#         company_pname_list = []
+#         for id in company_pid_list:
+#             property = Property.query.filter(Property.id == id).first()
+#             company_pname_list.append(property.name)
+#         com_dict[com.name] = company_pname_list
+#         data = {}
+#         data['company_id'] = com.id
+#         data['company_name'] = com.name
+#         data['company_category'] = [com.category]
+#         data['company_link'] = com.link
+#         data['company_cause'] = company_pname_list
+#         final_Data.append(data)
+#     response = jsonify({"data":
+#         {
+#             "code":200,
+#             "company_data":final_Data
+#         }
+#     })
+#     return response
+
+
+@home.route('/search', methods=['GET', 'POST'])
 @login_required
 def search():
     # form = SearchForm()
     data = request.get_json()
-    search = data['search']
-    company = Company.query.filter(Company.name.like('%' + search + '%'))
-    property =  Property.query.with_entities(Property.name).all()
-    property_list = [value for value, in property]
-    com_dict = {}
-    data = {}
-    final_Data = []
-    for com in company:
-        companyHasproperty = CompanyHasProperty.query.with_entities(
-            CompanyHasProperty.p_id).filter(
-                CompanyHasProperty.c_id == com.id).all()
-        companyHasproperty_pid = [value for value, in companyHasproperty]
-        company_pid_list = companyHasproperty_pid
-        company_pname_list = []
-        for id in company_pid_list:
-            property = Property.query.filter(Property.id == id).first()
-            company_pname_list.append(property.name)
-        com_dict[com.name] = company_pname_list
-        data = {}
-        data['company_id'] = com.id
-        data['company_name'] = com.name
-        data['company_category'] = [com.category]
-        data['company_link'] = com.link
-        data['company_cause'] = company_pname_list
-        final_Data.append(data)
+    search_company_name = data['search_company_name']
+    search_company_category = data['search_company_category']
+    search_company_causes = data['search_company_causes']
+    final_Data = search_company_based_on_filters(search_company_name, search_company_category, search_company_causes)
     response = jsonify({"data":
-        {
-            "code":200,
-            "company_data":final_Data
-        }
-    })
+            {
+                "code":200,
+                "company_data": final_Data
+            }
+        })
     return response
+    # if field == 'company_name':
+    #     final_Data = search_by_company_name(search=search)
+    # elif field == 'company_category':
+    #     final_Data = search_by_company_category(search=search)
+    # elif field == 'company_cause':
+    #     final_Data = search_by_company_cause(search=search)
+    # if final_Data == "Not found":
+    #     response = jsonify({"data":
+    #         {
+    #             "code": 400,
+    #             "error": final_Data
+    #         }
+    #     })
+    # else:
+    #     response = jsonify({"data":
+    #         {
+    #             "code":200,
+    #             "company_data":final_Data
+    #         }
+    #     })
+    # return response
     # return render_template('home/dashboard.html',form=form, company=company,
     #                         property_list=property_list,
     #                         com_dict=com_dict,
---
+++ app/utils/search_functions.py
@@ -0,0 +1,145 @@
+from ..models import Property,Company,CompanyHasProperty
+
+
+def search_by_company_name(search=None):
+    if search is None:
+        return "Not found"
+    company_id = []
+    company = Company.query.with_entities(
+            Company.id).filter(Company.name.like('%' + search + '%')).all()
+    for id, in company:
+        company_id.append(id)
+    return company_id
+    # property =  Property.query.with_entities(Property.name).all()
+    # property_list = [value for value, in property]
+    # com_dict = {}
+    # data = {}
+    # final_Data = []
+    # for com in company:
+    #     companyHasproperty = CompanyHasProperty.query.with_entities(
+    #         CompanyHasProperty.p_id).filter(
+    #             CompanyHasProperty.c_id == com.id).all()
+    #     companyHasproperty_pid = [value for value, in companyHasproperty]
+    #     company_pid_list = companyHasproperty_pid
+    #     company_pname_list = []
+    #     for id in company_pid_list:
+    #         property = Property.query.filter(Property.id == id).first()
+    #         company_pname_list.append(property.name)
+    #     com_dict[com.name] = company_pname_list
+    #     data = {}
+    #     data['company_id'] = com.id
+    #     data['company_name'] = com.name
+    #     data['company_category'] = [com.category]
+    #     data['company_link'] = com.link
+    #     data['company_cause'] = company_pname_list
+    #     final_Data.append(data)
+    # return final_Data
+
+
+def search_by_company_category(search=None):
+    if search is None:
+        return "Not Found"
+    company_id = []
+    company = Company.query.with_entities(
+            Company.id).filter(Company.category.in_(search)).all()
+    for id, in company:
+        company_id.append(id)
+    return company_id
+    # property =  Property.query.with_entities(Property.name).all()
+    # property_list = [value for value, in property]
+    # com_dict = {}
+    # final_Data = []
+    # for com in company:
+    #     companyHasproperty = CompanyHasProperty.query.with_entities(
+    #         CompanyHasProperty.p_id).filter(
+    #             CompanyHasProperty.c_id == com.id).all()
+    #     companyHasproperty_pid = [value for value, in companyHasproperty]
+    #     company_pid_list = companyHasproperty_pid
+    #     company_pname_list = []
+    #     for id in company_pid_list:
+    #         property = Property.query.filter(Property.id == id).first()
+    #         company_pname_list.append(property.name)
+    #     com_dict[com.name] = company_pname_list
+    #     data = {}
+    #     data['company_id'] = com.id
+    #     data['company_name'] = com.name
+    #     data['company_category'] = [com.category]
+    #     data['company_link'] = com.link
+    #     data['company_cause'] = company_pname_list
+    #     final_Data.append(data)
+    # return final_Data
+
+
+def search_by_company_cause(search=None):
+    if search is None:
+        return "Not Found"
+    property = Property.query.with_entities(Property.id).filter(Property.name.in_(search)).all()
+    property_id = []
+    for id, in property:
+        property_id.append(id)
+    company_has_property = CompanyHasProperty.query.with_entities(CompanyHasProperty.c_id).filter(
+        CompanyHasProperty.p_id.in_(property_id)).all()
+    company_has_property_list = []
+    for id, in company_has_property:
+        company_has_property_list.append(id)
+    company = Company.query.with_entities(Company.id).filter(Company.id.in_(company_has_property_list)).all()
+    company_id = []
+    for id, in company:
+        company_id.append(id)
+    return company_id
+
+
+def intersection(lst1, lst2):
+    temp = set(lst2)
+    lst3 = [value for value in lst1 if value in temp]
+    return lst3
+
+
+def prepare_response(company_id=None):
+    if company_id is None:
+        return "Not Found"
+    com_dict = {}
+    final_Data = []
+    company = Company.query.filter(Company.id.in_(company_id))
+    for com in company:
+        companyHasproperty = CompanyHasProperty.query.with_entities(
+            CompanyHasProperty.p_id).filter(
+                CompanyHasProperty.c_id == com.id).all()
+        companyHasproperty_pid = [value for value, in companyHasproperty]
+        company_pid_list = companyHasproperty_pid
+        company_pname_list = []
+        for id in company_pid_list:
+            property = Property.query.filter(Property.id == id).first()
+            company_pname_list.append(property.name)
+        com_dict[com.name] = company_pname_list
+        data = {}
+        data['company_id'] = com.id
+        data['company_name'] = com.name
+        data['company_category'] = [com.category]
+        data['company_link'] = com.link
+        data['company_cause'] = company_pname_list
+        final_Data.append(data)
+    return final_Data
+
+def search_company_based_on_filters(search_company_name=None,
+                                    search_company_category=None,
+                                    search_company_causes=None):
+    company_causes = []
+    company_category = []
+    if search_company_name != "":
+        company = search_by_company_name(search_company_name)
+        return prepare_response(company)
+
+    else:
+        if len(search_company_category) != 0:
+            company_category = search_by_company_category(search_company_category)
+            company_id = company_category
+
+        if len(search_company_causes) != 0:
+            company_causes = search_by_company_cause(search_company_causes)
+            company_id = company_causes
+
+        if len(company_category) != 0 and len(company_causes) != 0:
+            company_id = intersection(company_causes,company_category)
+
+        return prepare_response(company_id)
